af7dbfa7b7986d1bb21152bad499451f07521225
eythaann <eythan.cvt@gmail.com>
2024-12-21 16:51:39 -0500

    remove empty temporal weg items on sanitization

--- src/state/weg_items.rs
+++ src/state/weg_items.rs
@@ -95,7 +95,7 @@ impl WegItems {
                     }
                 }
                 WegItem::Temporal(data) => {
-                    if !data.path.exists() {
+                    if data.windows.len() == 0 || !data.path.exists() {
                         continue;
                     }
                     if data.execution_command.is_empty() {
