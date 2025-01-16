5ae532a13ca7c2316d5d386c2628d2b80feaeaf5
eythaann <eythan.cvt@gmail.com>
2025-01-16 09:55:50 -0500

    fix: circular dependencies

--- src/lib.ts
+++ src/lib.ts
@@ -1,8 +1,3 @@
-export * from './state/mod.ts';
-export * from './system_state/mod.ts';
-export * from './utils/mod.ts';
-export * from './handlers/mod.ts';
-
 import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow';
 import type { WidgetId } from '@seelen-ui/types';
 import { decodeBase64Url } from '@std/encoding/base64url';
@@ -16,7 +11,8 @@ interface WidgetInformation {
   params: Readonly<Record<string, string>>;
 }
 
-const CURRENT_WIDGET_INFORMATION: Readonly<WidgetInformation> = (() => {
+let CURRENT_WIDGET_INFORMATION: Readonly<WidgetInformation>;
+function _getCurrentWidget(): Readonly<WidgetInformation> {
   const encodedLabel = getCurrentWebviewWindow().label;
   const decodedLabel = new TextDecoder().decode(decodeBase64Url(encodedLabel));
   const [id, query] = decodedLabel.split('?');
@@ -28,8 +24,16 @@ const CURRENT_WIDGET_INFORMATION: Readonly<WidgetInformation> = (() => {
     rawLabel: encodedLabel,
     params: Object.freeze(Object.fromEntries(params)),
   });
-})();
+}
 
 export function getCurrentWidget(): Readonly<WidgetInformation> {
+  if (!CURRENT_WIDGET_INFORMATION) {
+    CURRENT_WIDGET_INFORMATION = _getCurrentWidget();
+  }
   return CURRENT_WIDGET_INFORMATION;
 }
+
+export * from './state/mod.ts';
+export * from './system_state/mod.ts';
+export * from './utils/mod.ts';
+export * from './handlers/mod.ts';
