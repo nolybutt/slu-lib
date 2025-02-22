ddbec3bf2b7c26bd142c368429d68fc5ef1e9e02
eythaann <eythan.cvt@gmail.com>
2025-02-22 06:54:04 -0500

    add new struct

--- src/system_state/ui_colors.rs
+++ src/system_state/ui_colors.rs
@@ -1,7 +1,9 @@
 use serde::Serialize;
+use ts_rs::TS;
 
 /// https://learn.microsoft.com/is-is/uwp/api/windows.ui.viewmanagement.uicolortype?view=winrt-19041
-#[derive(Debug, Default, Serialize)]
+#[derive(Debug, Default, Serialize, TS)]
+#[ts(export)]
 pub struct UIColors {
     pub background: String,
     pub foreground: String,
@@ -13,4 +15,49 @@ pub struct UIColors {
     pub accent_lighter: String,
     pub accent_lightest: String,
     pub complement: Option<String>,
+}
+
+/// since v2.2.0 this should be used to handle every used color in the app
+#[derive(Debug, Default, Serialize, TS)]
+#[ts(export)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ColorFormat {
+    Rgba(u32),
+    Rgb(u32),
+    Bgra(u32),
+    Bgr(u32),
+}
+
+impl Color {
+    pub fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
+        Self { r, g, b, a }
+    }
+
+    pub fn parse(format: ColorFormat) -> Self {
+        match format {
+            ColorFormat::Rgba(rgba) => {
+                let [r, g, b, a] = rgba.to_ne_bytes();
+                Color::new(r, g, b, a)
+            }
+            ColorFormat::Rgb(rgb) => {
+                let [_, r, g, b] = rgb.to_ne_bytes();
+                Color::new(r, g, b, 0xFF)
+            }
+            ColorFormat::Bgra(bgra) => {
+                let [b, g, r, a] = bgra.to_ne_bytes();
+                Color::new(r, g, b, a)
+            }
+            ColorFormat::Bgr(bgr) => {
+                let [_, b, g, r] = bgr.to_ne_bytes();
+                Color::new(r, g, b, 0xFF)
+            }
+        }
+    }
 }
--- src/system_state/ui_colors.ts
+++ src/system_state/ui_colors.ts
@@ -1,29 +1,21 @@
 import { SeelenCommand, SeelenEvent } from '../handlers/mod.ts';
 import { createInstanceInvoker, createInstanceOnEvent } from '../utils/State.ts';
+import type { Color as IColor, UIColors as IUIColors } from '@seelen-ui/types';
+
+export type { IColor, IUIColors };
 
 declare global {
   interface ArgsByCommand {
     [SeelenCommand.SystemGetColors]: null;
+    [SeelenCommand.SystemGetForegroundWindowColor]: null;
   }
   interface ReturnByCommand {
     [SeelenCommand.SystemGetColors]: IUIColors;
+    [SeelenCommand.SystemGetForegroundWindowColor]: IColor;
   }
   interface PayloadByEvent {
     [SeelenEvent.ColorsChanged]: IUIColors;
   }
-}
-
-export interface IUIColors {
-  background: string;
-  foreground: string;
-  accent_darkest: string;
-  accent_darker: string;
-  accent_dark: string;
-  accent: string;
-  accent_light: string;
-  accent_lighter: string;
-  accent_lightest: string;
-  complement: string | null;
 }
 
 export class UIColors {
@@ -47,7 +39,7 @@ export class UIColors {
     });
   }
 
-  setAssCssVariables(): void {
+  setAsCssVariables(): void {
     for (const [key, value] of Object.entries(this.inner)) {
       if (typeof value !== 'string') {
         continue;
@@ -62,5 +54,87 @@ export class UIColors {
       document.documentElement.style.setProperty(`--config-${name}-color`, value.slice(0, 7));
       document.documentElement.style.setProperty(`--config-${name}-color-rgb`, `${r}, ${g}, ${b}`);
     }
+  }
+}
+
+export class Color {
+  constructor(public inner: IColor) {}
+
+  private getDynamicStyleSheet(): HTMLStyleElement {
+    const styleId = 'sl-lib-dynamic-color-variables';
+    let styleElement = document.getElementById(styleId) as HTMLStyleElement;
+    if (!styleElement) {
+      styleElement = document.createElement('style');
+      styleElement.id = styleId;
+      styleElement.textContent = ':root {\n}';
+      document.head.appendChild(styleElement);
+    }
+    return styleElement;
+  }
+
+  private insertIntoStyleSheet(obj: Record<string, string>): void {
+    const sheet = this.getDynamicStyleSheet();
+    const lines = sheet.textContent!.split('\n');
+    lines.pop(); // remove the closing brace
+
+    for (const [key, value] of Object.entries(obj)) {
+      const old = lines.findIndex((line) => line.startsWith(key));
+      if (old !== -1) {
+        lines[old] = `${key}: ${value};`;
+      } else {
+        lines.push(`${key}: ${value};`);
+      }
+    }
+
+    lines.push('}');
+    sheet.textContent = lines.join('\n');
+  }
+
+  asHex(): string {
+    return (
+      '#' +
+      this.inner.r.toString(16).padStart(2, '0') +
+      this.inner.g.toString(16).padStart(2, '0') +
+      this.inner.b.toString(16).padStart(2, '0') +
+      this.inner.a.toString(16).padStart(2, '0')
+    );
+  }
+
+  /**
+   * @param name the name of the color
+   * the name will be parsed to lower kebab case and remove non-alphanumeric characters
+   * this will create some css variables as:\
+   * `--color-{name}` -> #RRGGBBAA\
+   * `--color-{name}-rgb` -> R, G, B
+   * `--color-{name}-rgba` -> R, G, B, A
+   */
+  setAsCssVariable(name: string): void {
+    const parsedName = name
+      .replace('_', '-')
+      .replace(/[^a-zA-Z0-9\-]/g, '')
+      .toLowerCase();
+
+    this.insertIntoStyleSheet({
+      [`--color-${parsedName}`]: this.asHex(),
+      [`--color-${parsedName}-rgb`]: `${this.inner.r}, ${this.inner.g}, ${this.inner.b}`,
+      [`--color-${parsedName}-rgba`]: `${this.inner.r}, ${this.inner.g}, ${this.inner.b}, ${this.inner.a}`,
+    });
+  }
+
+  /**
+   * https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
+   *
+   * @param accuracy if true will use an expensive but more accurate algorithm
+   * @returns a number between 0 and 255
+   */
+  calcLuminance(accuracy?: boolean): number {
+    const { r, g, b } = this.inner;
+    if (accuracy) {
+      const gR = r ** 2.2;
+      const gG = g ** 2.2;
+      const gB = b ** 2.2;
+      return (0.299 * gR + 0.587 * gG + 0.114 * gB) ** (1 / 2.2);
+    }
+    return 0.2126 * r + 0.7152 * g + 0.0722 * b;
   }
 }
