d4a4c44be302543dab3c49f33f66085185d3c3af
eythaann <eythan.cvt@gmail.com>
2025-06-01 15:49:07 -0500

    feat: add tauri plugins re-exports

--- src/re-exports/tauri.ts
+++ src/re-exports/tauri.ts
@@ -1,1 +1,7 @@
 export * from '@tauri-apps/api';
+export * from '@tauri-apps/api/dpi';
+
+export * as dialog from 'npm:@tauri-apps/plugin-dialog';
+export * as fs from 'npm:@tauri-apps/plugin-fs';
+export * as logger from 'npm:@tauri-apps/plugin-log';
+export * as process from 'npm:@tauri-apps/plugin-process';
